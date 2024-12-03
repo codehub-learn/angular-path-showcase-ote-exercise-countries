8b48e2c0199a321c169fe796efe952e7728c42a6
Ioannis Daniil <jondan97@gmail.com>
2024-12-03 10:11:26 +0200

    router basics

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { NotFound404Component } from './pages/not-found-404/not-found-404.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "home",
+        component: HomepageComponent
+    },
+    {
+        path: "",
+        redirectTo: "home",
+        pathMatch: "full"
+    },
+    {
+        path: "about-us",
+        component: AboutUsComponent
+    },
+    {
+        path: "**",
+        component: NotFound404Component
+    },
+];
